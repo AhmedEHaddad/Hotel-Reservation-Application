6189c1775bb3e4c9a83b8c0f8ef5d9176969f6e7
AhmedEHaddad <ahmed.e.h.2.55@gmail.com>
2022-06-24 22:42:56 +0300

    Add model classes

---
+++ Hotel-Reservation-App/src/model/FreeRoom.java
@@ -0,0 +1,13 @@
+package model;
+
+public class FreeRoom extends Room{
+    public FreeRoom(String roomNumber, Double price, RoomType roomType) {
+        super(roomNumber, 0.0, roomType);
+        //this.setPrice(0.0);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ Hotel-Reservation-App/src/model/Room.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Room implements IRoom{
+    private String roomNumber;
+    private Double price;
+    private RoomType roomType;
+
+    public Room(String roomNumber, Double price, RoomType roomType) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.roomType = roomType;
+    }
+
+
+
+    @Override
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    @Override
+    public double getRoomPrice() {
+        return price;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+    @Override
+    public String toString(){
+        return "Room no.  " + roomNumber + " Is a " + roomType + " room and costs" + price + " per night.";
+    }
+}
